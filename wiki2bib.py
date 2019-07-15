6fa9b360d4c1a73061d671d63d37595004b9c16f
tarikaltuncu <tarikaltuncu@gmail.com>
2019-07-15 01:08:04 +0100

    any vs. doi alternatives added. Also url accepts lists. any may require additional package. this branch needs refactoring before merging. bib file might not satisfy syntax if there exist an errornous anystyle entry

--- wiki2bib.py
+++ wiki2bib.py
@@ -4,14 +4,31 @@ import requests
 
 import os
 import re
-import sys
 
-crawl_url = sys.argv[1]
-pname = crawl_url.split('/')[-1]
+import argparse
 
-response = requests.get(crawl_url)
-html = response.text
-soup = bs4.BeautifulSoup(html, "html.parser")
+parser = argparse.ArgumentParser(description='Short sample app')
+
+parser.add_argument('--doi2bib', '-doi', action="store_true", default=True)
+parser.add_argument('--anystyle', '-any', action="store_true", default=False)
+parser.add_argument('-url', dest='url_list', nargs='+')
+# parser.add_argument('--pname', dest='pname_list', nargs='+')
+
+args = parser.parse_args()
+print(args)
+
+prefix = 'https://en.wikipedia.org/wiki/'
+references_list = list()
+for url in args.url_list:
+#     pname = url.split('/')[-1]
+    pname = url
+    crawl_url = prefix + pname
+    response = requests.get(crawl_url)
+    html = response.text
+    soup = bs4.BeautifulSoup(html, "html.parser")
+    
+    references_section = soup.find(['ol'], { "class" : "references"})
+    references_list.extend(references_section.find_all(['li']))
 
 doi_reg = r"\b(10[.][0-9]{4,}(?:[.][0-9]+)*(?:(?![\"&\'<>])\S)+)\b"
 doi_url = "http://dx.doi.org/"
@@ -20,10 +37,30 @@ try:
 	os.remove(f"./{pname+'.bib'}")
 except:
 	pass
+
+if args.anystyle:
+    f = open(f"./{pname+'.ref'}", 'w+')
 	
-references_section = soup.find(['ol'], { "class" : "references"})
-references_list = references_section.find_all(['li'])
+
 for ref in references_list:
-    dois = re.findall(doi_reg, str(ref))
-    for doi in dois:
-        os.system(f"doi2bib \"{doi_url + doi}\" >> \"{pname+'.bib'}\"")
+    
+    if args.doi2bib:
+        dois = re.findall(doi_reg, str(ref))
+        for doi in dois:
+            os.system(f"doi2bib \"{doi_url + doi}\" >> \"{pname+'.bib'}\"")
+    
+    if args.anystyle:
+        reference = ref.find_all("span", {'class':'reference-text'})[0].text
+        f.write(reference+'\n')
+        
+if args.anystyle:
+    f.close()
+
+    cmd = f"anystyle -f bib parse {pname}.ref >> \"{pname+'.bib'}\""
+    proc = os.popen(cmd)
+    proc.close()
+
+    try:
+        os.remove(f"./{pname+'.ref'}")
+    except:
+        pass
